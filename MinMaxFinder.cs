501d02f4022bb7ed173fbd7a170b105df0e1e343
Krzysztof Janiszewski <krzysztofjaniszewski@users.noreply.github.com>
2023-04-29 17:04:00 +0100

    refactor 1

---
+++ MinMaxFinder.cs
@@ -0,0 +1,150 @@
+﻿public sealed class MinMaxFinder
+{
+    ValueTuple<int, double>[][] _preCachedMinMax;
+
+    public MinMaxFinder(double[] array, bool isMax)
+    {
+        _preCachedMinMax = PreCacheMaxMin(array, isMax);
+    }
+
+    public static ValueTuple<int, double> Min(ValueTuple<int, double> a, ValueTuple<int, double> b)
+    {
+        return a.Item2 < b.Item2 ? a : b;
+    }
+
+    public static ValueTuple<int, double> Max(ValueTuple<int, double> a, ValueTuple<int, double> b)
+    {
+        return a.Item2 > b.Item2 ? a : b;
+    }
+
+    static ValueTuple<int, double>[][] PreCacheMaxMin(Span<double> rangeToInspect, bool isMax)
+    {
+        // const int nodeChildren = 2; // If changed, change base of log!
+        var lengthTotal = rangeToInspect.Length;
+        var treeLevels = (int)Math.Ceiling(1 + Math.Log2(lengthTotal));
+        var result = new ValueTuple<int, double>[treeLevels][];
+
+        if (result[0] == null) result[0] = new ValueTuple<int, double>[lengthTotal];
+        for (var i = 0; i < lengthTotal; i++)
+        {
+            result[0][i] = new ValueTuple<int, double>(i, rangeToInspect[i]);
+        }
+
+        for (var level = 1; level < treeLevels; level++)
+        {
+            var lengthAtThisLevel = (int)Math.Ceiling(lengthTotal / Math.Pow(2.0, level));
+            if (result[level] == null) result[level] = new ValueTuple<int, double>[lengthAtThisLevel];
+            for (var i = 0; i < lengthAtThisLevel; i++)
+            {
+                if (isMax)
+                {
+                    var best = new ValueTuple<int, double>(-1, double.MinValue);
+                    var loopEnd = Math.Min((i + 1) * 2, result[level - 1].Length);
+                    for (var j = i * 2; j < loopEnd; j++)
+                    {
+                        if (result[level - 1][j].Item2 > best.Item2)
+                        {
+                            best = result[level - 1][j];
+                        }
+                    }
+                    result[level][i] = best;
+                }
+                else
+                {
+                    var best = new ValueTuple<int, double>(-1, double.MaxValue);
+                    var loopEnd = Math.Min((i + 1) * 2, result[level - 1].Length);
+                    for (var j = i * 2; j < loopEnd; j++)
+                    {
+                        if (result[level - 1][j].Item2 < best.Item2)
+                        {
+                            best = result[level - 1][j];
+                        }
+                    }
+                    result[level][i] = best;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    public ValueTuple<int, double> GetMinMax(bool isMax, int start, int end)
+    {
+        var position = start;
+        var level = 0;
+        var best = _preCachedMinMax[0][start];
+        var dividerOnThisLevel = 1;
+        var nextLevelDistance = dividerOnThisLevel * 2;
+        var distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
+        var currentLevel = _preCachedMinMax[level];
+
+        if (isMax)
+        {
+            do
+            {
+                if (distanceToNextRoundNumber == 0 && position + nextLevelDistance <= end)
+                {
+                    level++;
+                    dividerOnThisLevel *= 2;
+                    nextLevelDistance *= 2;
+                    distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
+                    currentLevel = _preCachedMinMax[level];
+                    continue;
+                }
+                var positionPlusDividerOnThisLevel = position + dividerOnThisLevel;
+                if (positionPlusDividerOnThisLevel > end && level > 0)
+                {
+                    level--;
+                    dividerOnThisLevel /= 2;
+                    nextLevelDistance /= 2;
+                    distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
+                    currentLevel = _preCachedMinMax[level];
+                    continue;
+                }
+                var addressForThisLevel = position / dividerOnThisLevel;
+                var candidate = currentLevel[addressForThisLevel];
+                if (candidate.Item2 > best.Item2)
+                {
+                    best = candidate;
+                }
+                position = positionPlusDividerOnThisLevel;
+                distanceToNextRoundNumber -= dividerOnThisLevel;
+            } while (position <= end);
+            return best;
+        }
+
+        do
+        {
+            if (distanceToNextRoundNumber == 0 && position + nextLevelDistance <= end)
+            {
+                level++;
+                dividerOnThisLevel *= 2;
+                nextLevelDistance *= 2;
+                distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
+                currentLevel = _preCachedMinMax[level];
+                continue;
+            }
+            var positionPlusDividerOnThisLevel = position + dividerOnThisLevel;
+            if (positionPlusDividerOnThisLevel > end && level > 0)
+            {
+                level--;
+                dividerOnThisLevel /= 2;
+                nextLevelDistance /= 2;
+                distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
+                currentLevel = _preCachedMinMax[level];
+                continue;
+            }
+
+            var addressForThisLevel = position / dividerOnThisLevel;
+            var candidate = currentLevel[addressForThisLevel];
+            if (candidate.Item2 < best.Item2)
+            {
+                best = candidate;
+            }
+            position = positionPlusDividerOnThisLevel;
+            distanceToNextRoundNumber -= dividerOnThisLevel;
+        } while (position <= end);
+        return best;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
 using BenchmarkDotNet.Running;
 
 /*
-BenchmarkDotNet=v0.13.2, OS=Windows 11 (10.0.22621.900)
-AMD Ryzen Threadripper PRO 5995WX 64-Cores, 1 CPU, 128 logical and 64 physical cores
-.NET SDK=7.0.100
-  [Host]     : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
-  DefaultJob : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
+BenchmarkDotNet=v0.13.2, OS=Windows 11 (10.0.22621.1555)
+Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+.NET SDK=7.0.202
+  [Host]     : .NET 7.0.4 (7.0.423.11508), X64 RyuJIT AVX2
+  DefaultJob : .NET 7.0.4 (7.0.423.11508), X64 RyuJIT AVX2
 
 
-|       Method |       Mean |    Error |   StdDev |    Gen0 |    Gen1 |    Gen2 | Allocated |
-|------------- |-----------:|---------:|---------:|--------:|--------:|--------:|----------:|
-| WithPreCache |   370.7 us |  1.31 us |  1.22 us | 49.8047 | 49.8047 | 49.8047 | 391.53 KB |
-|        Naive | 7,558.8 us | 68.12 us | 63.72 us |       - |       - |       - |  78.45 KB |
+|       Method |       Mean |     Error |    StdDev |    Gen0 |    Gen1 |    Gen2 | Allocated |
+|------------- |-----------:|----------:|----------:|--------:|--------:|--------:|----------:|
+| WithPreCache |   476.5 us |   3.33 us |   2.78 us | 58.1055 | 49.8047 | 49.8047 | 391.56 KB |
+|        Naive | 9,663.5 us | 189.67 us | 202.94 us | 15.6250 |       - |       - |  78.45 KB |
 */
 
 PreCachedMinimumMaximum.TestCorrectness();
@@ -28,10 +28,10 @@ public class PreCachedMinimumMaximum
         var arr = GetRandomArrayForTest(rand, out int _, out int _, 10000);
         int startIdx = 0;
         int endIdx = arr.Length - 1;
-        var precached = PreCacheMaxMin(arr, true);
+        var precached = new MinMaxFinder(arr, true);
         for (var q = 0; q < 1000; q++)  // Pre-caching only makes sense when you need the min/max multiple times (otherwise it's slower)
         {
-            var max = GetMinMax(true, startIdx, endIdx, precached);
+            var max = precached.GetMinMax(true, startIdx, endIdx);
         }
     }
 
@@ -68,137 +68,6 @@ public class PreCachedMinimumMaximum
         return arr;
     }
 
-    static ValueTuple<int, double>[][] PreCacheMaxMin(Span<double> rangeToInspect, bool isMax)
-    {
-        // const int nodeChildren = 2; // If changed, change base of log!
-        var lengthTotal = rangeToInspect.Length;
-        var treeLevels = (int)Math.Ceiling(1 + Math.Log2(lengthTotal));
-        var result = new ValueTuple<int, double>[treeLevels][];
-
-        if (result[0] == null) result[0] = new ValueTuple<int, double>[lengthTotal];
-        for (var i = 0; i < lengthTotal; i++)
-        {
-            result[0][i] = new ValueTuple<int, double>(i, rangeToInspect[i]);
-        }
-
-        for (var level = 1; level < treeLevels; level++)
-        {
-            var lengthAtThisLevel = (int)Math.Ceiling(lengthTotal / Math.Pow(2.0, level));
-            if (result[level] == null) result[level] = new ValueTuple<int, double>[lengthAtThisLevel];
-            for (var i = 0; i < lengthAtThisLevel; i++)
-            {
-                if (isMax)
-                {
-                    var best = new ValueTuple<int, double>(-1, double.MinValue);
-                    var loopEnd = Math.Min((i + 1) * 2, result[level - 1].Length);
-                    for (var j = i * 2; j < loopEnd; j++)
-                    {
-                        if (result[level - 1][j].Item2 > best.Item2)
-                        {
-                            best = result[level - 1][j];
-                        }
-                    }
-                    result[level][i] = best;
-                }
-                else
-                {
-                    var best = new ValueTuple<int, double>(-1, double.MaxValue);
-                    var loopEnd = Math.Min((i + 1) * 2, result[level - 1].Length);
-                    for (var j = i * 2; j < loopEnd; j++)
-                    {
-                        if (result[level - 1][j].Item2 < best.Item2)
-                        {
-                            best = result[level - 1][j];
-                        }
-                    }
-                    result[level][i] = best;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    static ValueTuple<int, double> GetMinMax(bool isMax, int start, int end, ValueTuple<int, double>[][] preCachedMinMax)
-    {
-        var position = start;
-        var level = 0;
-        var best = preCachedMinMax[0][start];
-        var dividerOnThisLevel = 1;
-        var nextLevelDistance = dividerOnThisLevel * 2;
-        var distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
-        var currentLevel = preCachedMinMax[level];
-
-        if (isMax)
-        {
-            do
-            {
-                if (distanceToNextRoundNumber == 0 && position + nextLevelDistance <= end)
-                {
-                    level++;
-                    dividerOnThisLevel *= 2;
-                    nextLevelDistance *= 2;
-                    distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
-                    currentLevel = preCachedMinMax[level];
-                    continue;
-                }
-                var positionPlusDividerOnThisLevel = position + dividerOnThisLevel;
-                if (positionPlusDividerOnThisLevel > end && level > 0)
-                {
-                    level--;
-                    dividerOnThisLevel /= 2;
-                    nextLevelDistance /= 2;
-                    distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
-                    currentLevel = preCachedMinMax[level];
-                    continue;
-                }
-                var addressForThisLevel = position / dividerOnThisLevel;
-                var candidate = currentLevel[addressForThisLevel];
-                if (candidate.Item2 > best.Item2)
-                {
-                    best = candidate;
-                }
-                position = positionPlusDividerOnThisLevel;
-                distanceToNextRoundNumber -= dividerOnThisLevel;
-            } while (position <= end);
-            return best;
-        }
-
-        do
-        {
-            if (distanceToNextRoundNumber == 0 && position + nextLevelDistance <= end)
-            {
-                level++;
-                dividerOnThisLevel *= 2;
-                nextLevelDistance *= 2;
-                distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
-                currentLevel = preCachedMinMax[level];
-                continue;
-            }
-            var positionPlusDividerOnThisLevel = position + dividerOnThisLevel;
-            if (positionPlusDividerOnThisLevel > end && level > 0)
-            {
-                level--;
-                dividerOnThisLevel /= 2;
-                nextLevelDistance /= 2;
-                distanceToNextRoundNumber = (nextLevelDistance - position % nextLevelDistance) % nextLevelDistance;
-                currentLevel = preCachedMinMax[level];
-                continue;
-            }
-
-            var addressForThisLevel = position / dividerOnThisLevel;
-            var candidate = currentLevel[addressForThisLevel];
-            if (candidate.Item2 < best.Item2)
-            {
-                best = candidate;
-            }
-            position = positionPlusDividerOnThisLevel;
-            distanceToNextRoundNumber -= dividerOnThisLevel;
-        } while (position <= end);
-        return best;
-    }
-
-
     public static void TestCorrectness()
     {
         var rand = new Random(0);
@@ -215,8 +84,8 @@ public class PreCachedMinimumMaximum
                 }
             }
 
-            var precached = PreCacheMaxMin(arr, true);
-            var max = GetMinMax(true, startIdx, endIdx, precached);
+            var precached = new MinMaxFinder(arr, true);
+            var max = precached.GetMinMax(true, startIdx, endIdx);
             if (max.Item1 != best.Item1) throw new Exception();
             if (max.Item2 != best.Item2) throw new Exception();
         }
@@ -234,8 +103,8 @@ public class PreCachedMinimumMaximum
                 }
             }
 
-            var precached = PreCacheMaxMin(arr, false);
-            var min = GetMinMax(false, startIdx, endIdx, precached);
+            var precached = new MinMaxFinder(arr, false);
+            var min = precached.GetMinMax(false, startIdx, endIdx);
             if (min.Item1 != best.Item1) throw new Exception();
             if (min.Item2 != best.Item2) throw new Exception();
         }
